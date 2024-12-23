ead60ebb9fff15ab6981c06d24bd161b9ff4baf9
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2024-12-22 21:23:22 -0500

    Add entry for disabling frp initialization

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from log import Logger
 from minecraftServer import MCServer
+import sys
 
+bootArgs:list[str] = []
 logger:Logger = Logger("KSE")
 mcServer:MCServer = None
 
-def logStart():
+def logStart(args:list[str]):
     print("Krebostone KSE-Mini v0.1 created by kimsseTheWolf")
+    print(f"Booting Krebostone with arguments: {args}")
     logger.logInfo("Preparing for launch")
 
-def loadNecessary():
+def loadNecessary(args:list[str]):
     try:
         # Try to load minecraft server
         mcServer = MCServer()
         # Try to load frp server if necessary
+        if args.count("--no-frp") != 0:
+            pass
+        else:
+            logger.logInfo("FRP Server initialization skipped because of argument: --no-frp")
     except:
         logger.logError("Krebostone was exited because of critical error")
 
 if __name__ == "__main__":
+    bootArgs = sys.argv
     logStart()
     loadNecessary()
     
