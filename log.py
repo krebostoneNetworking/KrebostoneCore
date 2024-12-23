a55d97b7c09419b93f8e89579c4613c33092be76
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2024-12-22 20:48:30 -0500

    Create most basic logger

---
+++ log.py
@@ -0,0 +1,34 @@
+
+from colorama import Fore, Back, Style
+from datetime import datetime
+
+__debug_mode:bool = False
+
+def setDebugMode(isDebug:bool):
+    __debug_mode = isDebug
+
+def isDebugMode():
+    return __debug_mode
+
+DATE_TEMPLATE = "2022-01-01 12:00:00"
+
+class Logger:
+
+    def __init__(self, packageName:str):
+        self.packageName = packageName
+
+    def generateTimestamp(self):
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    def logInfo(self, msg:str):
+        print(f"[{Fore.GREEN}INFO{Fore.WHITE}][{self.generateTimestamp()}][{self.packageName}] {msg}")
+    
+    def logWarning(self, msg:str):
+        print(f"[{Fore.LIGHTRED_EX}INFO{Fore.WHITE}][{self.generateTimestamp()}][{self.packageName}] {msg}")
+
+    def logInfo(self, msg:str):
+        print(f"[{Fore.RED}INFO{Fore.WHITE}][{self.generateTimestamp()}][{self.packageName}] {msg}")
+
+    
+    
+    
