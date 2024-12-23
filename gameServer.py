ee36ba7e51b84c2a39127728a882143889b0bbd6
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2024-12-23 17:46:39 -0500

    Finish GameServer

--- gameServer.py
+++ gameServer.py
@@ -5,6 +5,19 @@ class GameServer(Launchable):
 
     def __init__(self, compName, exec, args):
         super().__init__(compName, exec, args)
-        self.__isMinecraftFullyLaunched:bool = False
+        self.isMinecraftFullyLaunched:bool = False
 
-    
+    def stop(self):
+        if not self.isRunning():
+            self.logger.logError(f"Service {self.compName} already stopped! No need for execution.")
+            return False
+        else:
+            self.send("stop")
+            self.isMinecraftFullyLaunched = False
+
+    def middleOperations(self, processedCmd:str):
+        # Monitoring if the game is started
+        if (processedCmd.count("Done!") > 0):
+            self.isMinecraftFullyLaunched = True
+            self.logger("Minecraft server fully booted! Have fun with your friends!")
+        pass
