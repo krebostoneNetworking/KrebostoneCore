996644764334921f36aae50ad7076c25a087310e
kimsseTheWolf <ilovemcmrcreeper@gmail.com>
2024-12-27 13:25:53 -0500

    Create basic non-abstract Service class

---
+++ service.py
@@ -0,0 +1,14 @@
+
+from launchable import Launchable
+
+class Service(Launchable):
+
+    def __init__(self, compName, wdir, exec, args):
+        super().__init__(compName, wdir, exec, args)
+
+    def stop(self):
+        if not self.isRunning():
+            self.logger.logError(f"Service {self.compName} already stopped! No need for execution.")
+            return False
+        else:
+            self.terminate()
